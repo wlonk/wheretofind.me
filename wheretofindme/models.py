25f0b310a84d2be6fa4b7eb37cacec6362a2eaf0
Kit La Touche <kit@transneptune.net>
2019-01-21 01:48:25 -0700

    Set secondary ordering on Follows
    Fixes #47.

--- wheretofindme/models.py
+++ wheretofindme/models.py
@@ -3,119 +3,86 @@ from urllib.parse import urlparse
 
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
 from django.db import models
 from django.urls import reverse
 
-from django.core.validators import URLValidator
-from django.core.exceptions import ValidationError
-
-ICONS = defaultdict(lambda: "fas fa-link", {
-    "facebook.com": "fab fa-facebook",
-    "www.facebook.com": "fab fa-facebook",
-
-    "github.com": "fab fa-github",
-    "www.github.com": "fab fa-github",
-
-    "instagram.com": "fab fa-instagram",
-    "www.instagram.com": "fab fa-instagram",
-
-    "keybase.io": "fab fa-keybase",
-    "www.keybase.io": "fab fa-keybase",
-
-    "mastodon.social": "fab fa-mastodon",
-
-    "plus.google.com": "fab fa-google-plus-g",
-
-    "twitch.tv": "fab fa-twitch",
-    "www.twitch.tv": "fab fa-twitch",
-
-    "twitter.com": "fab fa-twitter",
-    "www.twitter.com": "fab fa-twitter",
-
-    "youtube.com": "fab fa-youtube",
-    "www.youtube.com": "fab fa-youtube",
-
-    "behance.net": "fab fa-behance",
-    "www.behance.net": "fab fa-behance",
-
-    "bitbucket.org": "fab fa-bitbucket",
-    "www.bitbucket.org": "fab fa-bitbucket",
-
-    "deviantart.com": "fab fa-deviantart",
-    "www.deviantart.com": "fab fa-deviantart",
-
-    "pluspora.com": "fab fa-diaspora",
-    "www.pluspora.com": "fab fa-diaspora",
-
-    "dribbble.com": "fab fa-dribbble",
-    "www.dribbble.com": "fab fa-dribbble",
-
-    "ello.co": "fab fa-ello",
-    "www.ello.co": "fab fa-ello",
-
-    "codepen.io": "fab fa-codepen",
-    "www.codepen.io": "fab fa-codepen",
-
-    "discordapp.com": "fab fa-discord",
-    "www.discordapp.com": "fab fa-discord",
-
-    "vimeo.com": "fab fa-vimeo",
-    "www.vimeo.com": "fab fa-vimeo",
-
-    "untappd.com": "fab fa-untappd",
-    "www.untappd.com": "fab fa-untappd",
-
-    "tumblr.com": "fab fa-tumblr",
-    "www.tumblr.com": "fab fa-tumblr",
-
-    "teamspeak.com": "fab fa-teamspeak",
-    "www.teamspeak.com": "fab fa-teamspeak",
-
-    "steamcommunity.com": "fab fa-steam",
-    "www.steamcommunity.com": "fab fa-steam",
-
-    "stackoverflow.com": "fab fa-stackoverflow",
-    "www.stackoverflow.com": "fab fa-stackoverflow",
-
-    "soundcloud.com": "fab fa-soundcloud",
-    "www.soundcloud.com": "fab fa-soundcloud",
-
-    "snapchat.com": "fab fa-snapchat",
-    "www.snapchat.com": "fab fa-snapchat",
-
-    "slack.com": "fab fa-slack",
-    "www.slack.com": "fab fa-slack",
-
-    "skype.com": "fab fa-skype",
-    "www.skype.com": "fab fa-skype",
-
-    "reddit.com": "fab fa-reddit",
-    "www.reddit.com": "fab fa-reddit",
-
-    "pinterest.com": "fab fa-pinterest",
-    "www.pinterest.com": "fab fa-pinterest",
-
-    "patreon.com": "fab fa-patreon",
-    "www.patreon.com": "fab fa-patreon",
-
-    "paypal.com": "fab fa-paypal",
-    "www.paypal.com": "fab fa-paypal",
-
-    "medium.com": "fab fa-medium",
-    "www.medium.com": "fab fa-medium",
-
-    "kickstarter.com": "fab fa-kickstarter",
-    "www.kickstarter.com": "fab fa-kickstarter",
-
-    "goodreads.com": "fab fa-goodreads",
-    "www.goodreads.com": "fab fa-goodreads",
-
-    "gitlab.com": "fab fa-gitlab",
-    "www.gitlab.com": "fab fa-gitlab",
-
-    "etsy.com": "fab fa-etsy",
-    "www.etsy.com": "fab fa-etsy",
-})
+ICONS = defaultdict(
+    lambda: "fas fa-link",
+    {
+        "facebook.com": "fab fa-facebook",
+        "www.facebook.com": "fab fa-facebook",
+        "github.com": "fab fa-github",
+        "www.github.com": "fab fa-github",
+        "instagram.com": "fab fa-instagram",
+        "www.instagram.com": "fab fa-instagram",
+        "keybase.io": "fab fa-keybase",
+        "www.keybase.io": "fab fa-keybase",
+        "mastodon.social": "fab fa-mastodon",
+        "plus.google.com": "fab fa-google-plus-g",
+        "twitch.tv": "fab fa-twitch",
+        "www.twitch.tv": "fab fa-twitch",
+        "twitter.com": "fab fa-twitter",
+        "www.twitter.com": "fab fa-twitter",
+        "youtube.com": "fab fa-youtube",
+        "www.youtube.com": "fab fa-youtube",
+        "behance.net": "fab fa-behance",
+        "www.behance.net": "fab fa-behance",
+        "bitbucket.org": "fab fa-bitbucket",
+        "www.bitbucket.org": "fab fa-bitbucket",
+        "deviantart.com": "fab fa-deviantart",
+        "www.deviantart.com": "fab fa-deviantart",
+        "pluspora.com": "fab fa-diaspora",
+        "www.pluspora.com": "fab fa-diaspora",
+        "dribbble.com": "fab fa-dribbble",
+        "www.dribbble.com": "fab fa-dribbble",
+        "ello.co": "fab fa-ello",
+        "www.ello.co": "fab fa-ello",
+        "codepen.io": "fab fa-codepen",
+        "www.codepen.io": "fab fa-codepen",
+        "discordapp.com": "fab fa-discord",
+        "www.discordapp.com": "fab fa-discord",
+        "vimeo.com": "fab fa-vimeo",
+        "www.vimeo.com": "fab fa-vimeo",
+        "untappd.com": "fab fa-untappd",
+        "www.untappd.com": "fab fa-untappd",
+        "tumblr.com": "fab fa-tumblr",
+        "www.tumblr.com": "fab fa-tumblr",
+        "teamspeak.com": "fab fa-teamspeak",
+        "www.teamspeak.com": "fab fa-teamspeak",
+        "steamcommunity.com": "fab fa-steam",
+        "www.steamcommunity.com": "fab fa-steam",
+        "stackoverflow.com": "fab fa-stackoverflow",
+        "www.stackoverflow.com": "fab fa-stackoverflow",
+        "soundcloud.com": "fab fa-soundcloud",
+        "www.soundcloud.com": "fab fa-soundcloud",
+        "snapchat.com": "fab fa-snapchat",
+        "www.snapchat.com": "fab fa-snapchat",
+        "slack.com": "fab fa-slack",
+        "www.slack.com": "fab fa-slack",
+        "skype.com": "fab fa-skype",
+        "www.skype.com": "fab fa-skype",
+        "reddit.com": "fab fa-reddit",
+        "www.reddit.com": "fab fa-reddit",
+        "pinterest.com": "fab fa-pinterest",
+        "www.pinterest.com": "fab fa-pinterest",
+        "patreon.com": "fab fa-patreon",
+        "www.patreon.com": "fab fa-patreon",
+        "paypal.com": "fab fa-paypal",
+        "www.paypal.com": "fab fa-paypal",
+        "medium.com": "fab fa-medium",
+        "www.medium.com": "fab fa-medium",
+        "kickstarter.com": "fab fa-kickstarter",
+        "www.kickstarter.com": "fab fa-kickstarter",
+        "goodreads.com": "fab fa-goodreads",
+        "www.goodreads.com": "fab fa-goodreads",
+        "gitlab.com": "fab fa-gitlab",
+        "www.gitlab.com": "fab fa-gitlab",
+        "etsy.com": "fab fa-etsy",
+        "www.etsy.com": "fab fa-etsy",
+    },
+)
 
 
 class UserQuerySet(models.QuerySet):
@@ -142,13 +109,8 @@ class User(AbstractUser):
 class InternetIdentity(models.Model):
     class Meta:
         verbose_name_plural = "Internet Identities"
-        ordering = (
-            "seq",
-            "created_at",
-        )
-        unique_together = [
-            ("user", "seq"),
-        ]
+        ordering = ("seq", "created_at")
+        unique_together = [("user", "seq")]
 
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -176,12 +138,8 @@ class InternetIdentity(models.Model):
 
 class Follow(models.Model):
     class Meta:
-        ordering = (
-            "to_user__username",
-        )
-        unique_together = (
-            ("from_user", "to_user"),
-        )
+        ordering = ("to_user__username", "from_user__username")
+        unique_together = (("from_user", "to_user"),)
 
     from_user = models.ForeignKey(User, on_delete=models.PROTECT)
     to_user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="+")
