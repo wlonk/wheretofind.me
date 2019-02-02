43e30bc32fc495e76be16901e02f0a2fe50624de
Kit La Touche <kit@transneptune.net>
2019-02-01 20:09:15 -0700

    Disallow usernames that differ only by case

--- wheretofindme/forms.py
+++ wheretofindme/forms.py
@@ -1,12 +1,10 @@
+from crispy_forms.bootstrap import PrependedText
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Field, Layout, Submit
 from django import forms
 from django.utils.translation import gettext_lazy as _
-
 from django_registration import validators
 from django_registration.forms import RegistrationFormUniqueEmail
-
-from crispy_forms.bootstrap import PrependedText, Alert
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit, Field
 
 from .models import User
 
@@ -18,28 +16,31 @@ class CustomUserForm(RegistrationFormUniqueEmail):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields[User.USERNAME_FIELD].validators.append(
+            validators.CaseInsensitiveUnique(
+                User, User.USERNAME_FIELD, validators.DUPLICATE_USERNAME
+            )
+        )
         self.fields["username"].help_text += (
             " You cannot change this without intervention from an admin, so "
             "consider carefully."
         )
-        self.fields["tos"].label = (
-            "I have read and agree to the <a href='/tos/'>Terms of Service</a>"
-        )
+        self.fields[
+            "tos"
+        ].label = "I have read and agree to the <a href='/tos/'>Terms of Service</a>"
         self.helper = FormHelper()
-        self.helper.form_action = 'django_registration_register'
+        self.helper.form_action = "django_registration_register"
         self.helper.layout = Layout(
             PrependedText("username", "https://wheretofind.me/@"),
             Field("email"),
             Field("password1"),
             Field("password2"),
             Field("tos"),
-            Submit('save', 'Sign up', css_class="btn btn-primary"),
+            Submit("save", "Sign up", css_class="btn btn-primary"),
         )
 
     tos = forms.BooleanField(
         widget=forms.CheckboxInput,
-        label=_(u'I have read and agree to the Terms of Service'),
-        error_messages={
-            'required': validators.TOS_REQUIRED,
-        }
+        label=_(u"I have read and agree to the Terms of Service"),
+        error_messages={"required": validators.TOS_REQUIRED},
     )
