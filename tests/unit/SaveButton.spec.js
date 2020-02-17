4775907310a71ecdc52fd2d9fde2a3f8a0049f7b
toBeOfUse <mitchjacov@gmail.com>
2020-02-16 22:29:06 -0500

    tests for SaveButton component

---
+++ tests/unit/SaveButton.spec.js
@@ -0,0 +1,32 @@
+import { shallowMount } from '@vue/test-utils';
+import SaveButton from '@/components/SaveButton.vue';
+
+describe('SaveButton.vue', () => {
+  const propsData = {
+    allUploadsComplete: true,
+  };
+  const wrapper = shallowMount(SaveButton, { propsData });
+  test('Goes into spinnning mode after click and stops after 400ms (unless already spinning because !allUploadsComplete)', () => {
+    jest.useFakeTimers();
+    wrapper.find('button').trigger('click');
+    expect(wrapper.vm.justBeenClicked).toBe(true);
+    expect(wrapper.vm.spinning).toBe(true);
+    jest.advanceTimersByTime(401);
+    expect(wrapper.vm.justBeenClicked).toBe(false);
+    wrapper.setProps({ allUploadsComplete: false });
+    wrapper.find('button').trigger('click');
+    expect(wrapper.vm.justBeenClicked).toBe(false);
+  });
+  test('Goes into spinning mode when !allUploadsComplete', () => {
+    wrapper.setProps({ allUploadsComplete: false });
+    expect(wrapper.vm.spinning).toBe(true);
+    wrapper.setProps({ allUploadsComplete: true });
+    expect(wrapper.vm.spinning).toBe(false);
+  });
+  test('Button is disabled when !allUploadsComplete', async () => {
+    wrapper.setProps({ allUploadsComplete: false });
+    const button = wrapper.find('button');
+    await wrapper.vm.$nextTick();
+    expect(button.element.disabled).toBe(true);
+  });
+});
